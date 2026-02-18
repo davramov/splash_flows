df402d66a5484037a0ee3faef446f80e1e828fd6
David Abramov <dabramov@lbl.gov>
2026-02-18 14:11:39 -0800

    Updating the combine step to use the dino/cellpose env

--- orchestration/flows/bl832/alcf.py
+++ orchestration/flows/bl832/alcf.py
@@ -775,7 +775,7 @@ class ALCFTomographyHPCController(TomographyHPCController):
             master_addr = "localhost"
             print("No PBS_NODEFILE, single node mode")
 
-        venv_path = "/eagle/SYNAPS-I/segmentation/env_dino_cellpose""
+        venv_path = "/eagle/SYNAPS-I/segmentation/env_dino_cellpose"
 
         # Build command as a list
         cmd_list = [
@@ -1019,7 +1019,7 @@ class ALCFTomographyHPCController(TomographyHPCController):
         combine_start = time.time()
         os.chdir(workdir)
 
-        venv_path = "/eagle/SYNAPS-I/segmentation/env"
+        venv_path = "/eagle/SYNAPS-I/segmentation/env_dino_cellpose"
 
         env = os.environ.copy()
         env.update({
