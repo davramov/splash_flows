021c0b4541898a0bfc87c5c52dd15002f18c0a1b
David Abramov <dabramov@lbl.gov>
2026-02-18 14:07:56 -0800

    updating ALCF conda env for dino/cellpose

--- orchestration/flows/bl832/alcf.py
+++ orchestration/flows/bl832/alcf.py
@@ -775,7 +775,7 @@ class ALCFTomographyHPCController(TomographyHPCController):
             master_addr = "localhost"
             print("No PBS_NODEFILE, single node mode")
 
-        venv_path = "/eagle/SYNAPS-I/segmentation/env"
+        venv_path = "/eagle/SYNAPS-I/segmentation/env_dino_cellpose""
 
         # Build command as a list
         cmd_list = [
@@ -884,7 +884,7 @@ class ALCFTomographyHPCController(TomographyHPCController):
             master_addr = "localhost"
             print("No PBS_NODEFILE, single node mode")
 
-        venv_path = "/eagle/SYNAPS-I/segmentation/env"
+        venv_path = "/eagle/SYNAPS-I/segmentation/env_dino_cellpose"
 
         # Build command as a list
         cmd_list = [
