9824c185d861aaa4f4229d6b1924f2bbe13c0e46
David Abramov <dabramov@lbl.gov>
2026-04-06 13:07:42 -0700

    Adding mlflow uris to config

--- orchestration/flows/bl832/config.py
+++ orchestration/flows/bl832/config.py
@@ -29,6 +29,8 @@ class Config832(BeamlineConfig):
         self.alcf832_scratch = self.endpoints["alcf832_scratch"]
         # SciCat
         self.scicat = self.config["scicat"]
+        # MLflow
+        self.mlflow = self.config["mlflow"]["local"]
         # NERSC HPC submission settings
         self.ghcr_images832 = self.config["ghcr_images832"]
         self.nersc_recon_settings = self.config["hpc_submission_settings832"]["nersc_reconstruction"]
