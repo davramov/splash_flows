6c703f08da0861d44353e48c09965bef102e420c
David Abramov <dabramov@lbl.gov>
2026-05-20 11:31:59 -0700

    Adding beegfs/bl832/scratch endpoint

--- orchestration/flows/bl832/config.py
+++ orchestration/flows/bl832/config.py
@@ -28,6 +28,7 @@ class Config832(BeamlineConfig):
         self.alcf832_raw = self.endpoints["alcf832_raw"]
         self.alcf832_scratch = self.endpoints["alcf832_scratch"]
         self.beegfs_raw = self.endpoints["bl832-beegfs-raw"]
+        self.beegfs_scratch = self.endpoints["bl832-beegfs-scratch"]
         # SciCat
         self.scicat = self.config["scicat"]
         # MLflow
