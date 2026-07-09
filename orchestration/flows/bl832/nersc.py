9bddc0a33f16e73560bc3aa14ff5aafc91b0dc3d
David Abramov <dabramov@lbl.gov>
2026-07-09 14:45:52 -0700

    Trying to ingest up 1 level to match what worked with the cli ingestion

--- orchestration/flows/bl832/nersc.py
+++ orchestration/flows/bl832/nersc.py
@@ -1923,7 +1923,7 @@ def nersc_recon_flow(
     # Register the reconstructed ZARRs in tiled
     register_file_to_tiled(
         # path=Path(config.beegfs_scratch.root_path+zarr_file_path),
-        path=Path("/global/beegfs/beamlines/bl832/" + config.beegfs_scratch.root_path + zarr_file_path),
+        path=Path("/global/beegfs/beamlines/bl832/" + config.beegfs_scratch.root_path + f"/{folder_name}"),
         prefix=f"beamlines/bl832/processed/{folder_name}",
         overwrite=True,
         tags=["8.3.2", folder_name],
